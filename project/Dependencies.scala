f8775eded7b053498a88c33114f239236dc3ac54
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-12-13 08:32:52 +0000

    Update slf4j-simple to 2.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,6 +17,6 @@ object Dependencies {
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.2"
   lazy val pureConfigCatsEffect = "com.github.pureconfig" %% "pureconfig-cats-effect" % "0.17.2"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
-  lazy val slf4j = "org.slf4j" % "slf4j-simple" % "2.0.5"
+  lazy val slf4j = "org.slf4j" % "slf4j-simple" % "2.0.6"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "2.27.2"
 }
