f0e1516d0f264ed04c5b12f93ada65408b27b639
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-10-10 08:35:47 +0000

    Update keycloak-admin-client, ... to 22.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  private val keycloakVersion = "22.0.3"
+  private val keycloakVersion = "22.0.4"
   private val awsUtilsVersion = "0.1.106"
 
   lazy val kmsUtils =  "uk.gov.nationalarchives" %% "kms-utils" % awsUtilsVersion
