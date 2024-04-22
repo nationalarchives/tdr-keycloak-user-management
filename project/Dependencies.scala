c4cdc9f702efe15343e4d8bd2f5d949ba7e5e072
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-04-22 08:27:43 +0000

    Update decoders-utils, kms-utils, s3-utils to 0.1.161 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   private val keycloakVersion = "24.0.3"
-  private val awsUtilsVersion = "0.1.160"
+  private val awsUtilsVersion = "0.1.161"
 
   lazy val kmsUtils =  "uk.gov.nationalarchives" %% "kms-utils" % awsUtilsVersion
   lazy val s3Utils =  "uk.gov.nationalarchives" %% "s3-utils" % awsUtilsVersion
