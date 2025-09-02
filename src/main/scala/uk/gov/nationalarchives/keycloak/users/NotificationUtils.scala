e2480de7ac15ee40312e981c8b5e969bf569eb58
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2025-09-02 08:01:48 +0100

    [TDRD-981] Send users disabled event from inactive keycloak users lambda

---
+++ src/main/scala/uk/gov/nationalarchives/keycloak/users/NotificationUtils.scala
@@ -0,0 +1,21 @@
+package uk.gov.nationalarchives.keycloak.users
+
+import io.circe.generic.auto._
+import io.circe.syntax.EncoderOps
+import software.amazon.awssdk.services.sns.model.PublishResponse
+import uk.gov.nationalarchives.aws.utils.sns.SNSUtils
+import uk.gov.nationalarchives.aws.utils.sns.SNSClients.sns
+import uk.gov.nationalarchives.keycloak.users.Config.Sns
+import uk.gov.nationalarchives.keycloak.users.InactiveKeycloakUsersLambda.LogInfo
+import uk.gov.nationalarchives.keycloak.users.NotificationUtils.UsersDisabledEvent
+
+
+class NotificationUtils(snsUtils: SNSUtils, snsConfig: Sns, environment: String) {
+  def publishUsersDisabledEvent(count: Integer, logInfo: LogInfo): PublishResponse =
+    snsUtils.publish(UsersDisabledEvent(environment, count, logInfo).asJson.toString(), snsConfig.notificationsTopicArn)
+}
+
+object NotificationUtils {
+  def apply(snsConfig: Sns, environment: String): NotificationUtils = new NotificationUtils(SNSUtils(sns(snsConfig.endpoint)), snsConfig, environment)
+  case class UsersDisabledEvent(environment: String, disabledUsersCount: Int, logInfo: LogInfo)
+}
